cce3897ab7aed7648e0c90d76ca4d10fad91031b
Vitalii <vetall4444@gmail.com>
2020-06-12 02:46:56 +0500

    На этапе все кнопки кроме последней

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),   
+notify = require("gulp-notify"),  
+nib = require("nib"),  
+plumber = require('gulp-plumber'),
+stylus = require('gulp-stylus'),  a
+autoprefixer = require('gulp-autoprefixer'),
+cssmin = require('gulp-cssmin');
+gulp.task('stylus', function() {return gulp.src(['./style/styl/*.styl'])
+.pipe(plumber({      errorHandler: notify.onError()    }))   .pipe(stylus({      use: nib(),      compress: false,    }))  
+.pipe(autoprefixer({      browsers: ['last 15 versions'],    }))    
+.pipe(gulp.dest('./style/css')) 
+.pipe(notify('Stylus SUCCESS'));});
+gulp.task('watch', function() {  gulp.watch(['./style/styl/*.styl'], ['stylus']);});
